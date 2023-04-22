38758a787e7546b714a4dec2f972ce1fcf43a490
Adytzu <104699007+adytzuPWN@users.noreply.github.com>
2023-04-22 18:01:22 +0300

    Syntax pawn
    I left the default files

---
+++ OpenWikiOnString.py
@@ -0,0 +1,9 @@
+import sublime
+import sublime_plugin
+import webbrowser
+
+
+class OpenWikiOnString(sublime_plugin.TextCommand):
+    def run(self, edit):
+        query = self.view.substr(self.view.sel()[0])
+        webbrowser.open_new("http://wiki.sa-mp.com/wiki/" + query)
